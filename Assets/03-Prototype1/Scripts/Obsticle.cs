9a0c483e084fa08bd3e3253b24c6f41d18827dec
Joan Rosario <g00283924@stu.raritanval.edu>
2021-10-05 12:50:23 -0400

    Game Update
    Added obstacles that subtract points from score when it collides with projectile. Modified slingshot to prevent spamming of projectile

---
+++ Assets/03-Prototype1/Scripts/Obsticle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Obsticle : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        scoreGT = scoreGO.gameObject.GetComponent<Text>();
+
+        scoreGT.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+
+        if (collidedWith.tag == "Projectile")
+        {
+            Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+
+            score += -50;
+
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,8 @@ player shoots for the target
 Using the scripts from previous prototype I will create a game with
 some of the mechanics used for apple picker, roll a ball and mission demolition.
 
+Game Concept: 
+Shoot to hit the target. Obsticles will destroy projectiles 
+and subtract points. Hitting Obsticle will subtract 50 points. While hitting 
+target will gain 100 points
+
